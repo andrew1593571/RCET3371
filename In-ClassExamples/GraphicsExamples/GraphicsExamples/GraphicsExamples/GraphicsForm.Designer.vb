67ba4a13a9a16372c50640f3fe4ff90cd71576e0
andrew1593571 <andrewkeller2002@gmail.com>
2025-02-04 09:35:46 -0700

    GUI Design

--- In-ClassExamples/GraphicsExamples/GraphicsExamples/GraphicsExamples/GraphicsForm.Designer.vb
+++ In-ClassExamples/GraphicsExamples/GraphicsExamples/GraphicsExamples/GraphicsForm.Designer.vb
@@ -22,10 +22,70 @@ Partial Class GraphicsForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.DrawButton = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(471, 258)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(386, 276)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(97, 38)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(283, 276)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(97, 38)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'DrawButton
+        '
+        Me.DrawButton.Location = New System.Drawing.Point(180, 276)
+        Me.DrawButton.Name = "DrawButton"
+        Me.DrawButton.Size = New System.Drawing.Size(97, 38)
+        Me.DrawButton.TabIndex = 3
+        Me.DrawButton.Text = "&Draw"
+        Me.DrawButton.UseVisualStyleBackColor = True
+        '
+        'GraphicsForm
+        '
+        Me.AcceptButton = Me.DrawButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(495, 323)
+        Me.Controls.Add(Me.DrawButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "GraphicsForm"
+        Me.Text = "Graphics!"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents DrawButton As Button
 End Class
