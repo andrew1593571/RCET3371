e5cd3f9ae658abdff170a6d0e6fb75a1eeee78d2
andrew1593571 <andrewkeller2002@gmail.com>
2025-03-05 08:52:44 -0700

    Shuffle and New Subs

--- GameOfWar/GameOfWar/GameOfWar/CardGameClasses/DeckOfCards.vb
+++ GameOfWar/GameOfWar/GameOfWar/CardGameClasses/DeckOfCards.vb
@@ -24,4 +24,62 @@ Public Class DeckOfCards
         End Get
     End Property
 
+    ''' <summary>
+    ''' When a new instance is created, shuffle the deck
+    ''' </summary>
+    Sub New()
+        Me.Shuffle()
+    End Sub
+
+    ''' <summary>
+    ''' Load the deck stack with a shuffled deck
+    ''' </summary>
+    Public Sub Shuffle()
+        Dim suit As Integer
+        Dim value As Integer
+        Dim card As PlayingCard
+        Dim deckArray() As PlayingCard
+        Dim inDeck As Boolean
+
+        Me._deck.Clear()
+
+        For i = 1 To 52
+            suit = Me.GetRandomNumberInRange(4, 1)
+            value = Me.GetRandomNumberInRange(13, 1)
+            inDeck = False
+
+            card = New PlayingCard(suit, value)
+            deckArray = Me._deck.ToArray()
+
+            For j = 0 To deckArray.Length() - 1
+                If suit = deckArray(j).Suit And value = deckArray(j).Value Then
+                    inDeck = True
+                End If
+            Next
+
+            If inDeck Then
+                i -= 1
+            Else
+                Me._deck.Push(card)
+            End If
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Returns a random number in the specified range
+    ''' </summary>
+    ''' <param name="Max"></param>
+    ''' <param name="Min"></param>
+    ''' <returns></returns>
+    Private Function GetRandomNumberInRange(Max As Integer, Optional Min As Integer = 0) As Integer
+        Dim randomNumber As Integer
+
+        'Initialize randomize with the current time in milliseconds
+        Randomize(DateTime.Now.Millisecond)
+
+        randomNumber = CInt(Math.Floor((Rnd() * ((Max - Min) + 1)))) + Min
+
+        Return randomNumber
+    End Function
+
 End Class
