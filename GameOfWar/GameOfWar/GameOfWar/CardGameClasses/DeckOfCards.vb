2027e2874e0564fa2ba7e3db3b45351ec93ca4c8
andrew1593571 <andrewkeller2002@gmail.com>
2025-03-05 08:45:42 -0700

    Deck stack, remaining cards property

--- GameOfWar/GameOfWar/GameOfWar/CardGameClasses/DeckOfCards.vb
+++ GameOfWar/GameOfWar/GameOfWar/CardGameClasses/DeckOfCards.vb
@@ -9,4 +9,19 @@ Option Strict On
 
 Public Class DeckOfCards
 
+    ''' <summary>
+    ''' Stores the shuffled deck in a stack
+    ''' </summary>
+    Private _deck As New Stack(Of PlayingCard)
+
+    ''' <summary>
+    ''' The number of remaining cards in the deck
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property Remaining() As Integer
+        Get
+            Return Me._deck.Count
+        End Get
+    End Property
+
 End Class
