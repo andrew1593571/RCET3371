a3951c9590ebac4004f9733ff1363d84c7db4395
andrew1593571 <andrewkeller2002@gmail.com>
2025-03-05 08:41:55 -0700

    Init DeckOfCards Class

---
+++ GameOfWar/GameOfWar/GameOfWar/CardGameClasses/DeckOfCards.vb
@@ -0,0 +1,12 @@
+﻿Option Explicit On
+Option Strict On
+
+'Andrew Keller
+'RCET3371
+'Spring 2025
+'GameOfWar
+'https://github.com/andrew1593571/RCET3371.git
+
+Public Class DeckOfCards
+
+End Class
